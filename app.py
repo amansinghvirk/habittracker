ee43e45a72358dfd69934cdd4c1967fb18973483
amansinghvirk <aman.virk.ops@gmail.com>
2023-08-22 20:15:04 -0400

    Database moved to mongodb!

--- app.py
+++ app.py
@@ -1,59 +1,22 @@
 import os
-import datetime
-from collections import defaultdict
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
+from routes import pages
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
+load_dotenv()
 
+def mongo_string():
+    return os.getenv("MONGODB_URI")
 
+def habittracker_db():
+    client = MongoClient(mongo_string())
+    db = client.habittracker
+    return db
 
 def create_app():
     app = Flask(__name__)
-
-    habits = ["Test habit", "Test habit 2"]
-    completions = defaultdict(list)
-
-    @app.context_processor
-    def add_calc_date_range():
-        def date_range(start: datetime.date):
-            dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
-            return dates
-        
-        return {"date_range": date_range}
-
-    @app.route('/')
-    def index():
-        date_str = request.args.get("date")
-        if date_str:
-            selected_date = datetime.date.fromisoformat(date_str)
-        else:
-            selected_date = datetime.date.today()
-
-        return render_template(
-            "index.html", 
-            habits=habits, 
-            title="Habit Tracker - Home",
-            completions=completions[selected_date],
-            selected_date=selected_date
-        )
-    
-    @app.route("/add", methods=["GET", "POST"])
-    def add_habit():
-        if request.method == "POST":
-            habit = request.form.get("habit")
-            habits.append(habit)
-        return render_template(
-            "add_habit.html", 
-            title="Habit Tracker - Add Habit", 
-            selected_date=datetime.date.today()
-        )
-
-    @app.route("/complete", methods=["POST"])
-    def complete():
-        date_string = request.form.get("date")
-        habit = request.form.get("habitName")
-        date = datetime.date.fromisoformat(date_string)
-        completions[date].append(habit)
-
-        return redirect(url_for("index", date=date_string))
+    app.db = habittracker_db()
+    app.register_blueprint(pages)
     
     return app
